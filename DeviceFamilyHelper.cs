35a19e5e837e04e312153938c23a59f8c4a458fe
Leeway <leewayzhang@outlook.com>
2016-06-15 15:27:44 +0800

    add DeviceFamilyHelper

---
+++ DeviceFamilyHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+namespace UWPHelpers
+{
+    public static class DeviceFamilyHelper
+    {
+        public static DeviceFamilyType GetDeviceFamilyType()
+        {
+            switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                case "Windows.Mobile":
+                    return DeviceFamilyType.Phone;
+                case "Windows.Desktop":
+                    return UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse ? DeviceFamilyType.Desktop : DeviceFamilyType.Tablet;
+                case "Windows.Universal":
+                    return DeviceFamilyType.IoT;
+                case "Windows.Team":
+                    return DeviceFamilyType.SurfaceHub;
+                default:
+                    return DeviceFamilyType.Other;
+            }
+        }
+    }
+
+    public enum DeviceFamilyType
+    {
+        Phone,
+        Desktop,
+        Tablet,
+        IoT,
+        SurfaceHub,
+        Other
+    }
+}
